228c4c5d43b91a8137591a77a12f8a5690574b05
Tomasz Kluza <kluza.tomasz@gmail.com>
2015-11-21 17:44:40 +0100

    Login UI in progress

--- MyApplication/app/src/main/java/pl/checkersmobile/gui/LoginActivity.java
+++ MyApplication/app/src/main/java/pl/checkersmobile/gui/LoginActivity.java
@@ -4,14 +4,19 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import pl.checkersmobile.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-    @Bind(R.id.hello_world)
-    TextView mHelloWorld;
+    @Bind(R.id.activity_login_tvLogin)
+    TextView tvLogin;
+    @Bind(R.id.activity_login_tvRegister)
+    TextView tvRegister;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
