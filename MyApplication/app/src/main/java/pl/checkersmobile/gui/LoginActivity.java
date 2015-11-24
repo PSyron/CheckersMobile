c6fa6a40edf4110961abe08c7e58ca7fefd7b285
tkluza <kluza.tomasz@gmail.com>
2015-11-24 21:53:43 +0100

    fix

--- MyApplication/app/src/main/java/pl/checkersmobile/gui/LoginActivity.java
+++ MyApplication/app/src/main/java/pl/checkersmobile/gui/LoginActivity.java
@@ -1,5 +1,6 @@
 package pl.checkersmobile.gui;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -10,12 +11,16 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import pl.checkersmobile.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
 
+    @Bind(R.id.activity_login_btnLogin)
+    TextView btnLogin;
+    @Bind(R.id.activity_login_btnRegister)
+    TextView btnRegister;
     @Bind(R.id.activity_login_tvLogin)
     TextView tvLogin;
-    @Bind(R.id.activity_login_tvRegister)
-    TextView tvRegister;
+    @Bind(R.id.activity_login_tvPassword)
+    TextView tvPassword;
 
 
     @Override
