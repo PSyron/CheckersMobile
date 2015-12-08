32f9bdd3eef45ef46ac913913db864acfec730d0
Paweł Cyroń <paul.cyron1@gmail.com>
2015-12-08 20:49:16 +0100

    fix na glupote

--- MyApplication/app/src/main/java/pl/checkersmobile/gui/BaseAppBarActivity.java
+++ MyApplication/app/src/main/java/pl/checkersmobile/gui/BaseAppBarActivity.java
@@ -1,11 +1,7 @@
 package pl.checkersmobile.gui;
 
-import android.app.ActionBar;
-import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 
 import pl.checkersmobile.R;
 
@@ -17,13 +13,6 @@ public class BaseAppBarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
-    }
-
-    public boolean onOptionsItemSelected(MenuItem item){
-        Intent myIntent = new Intent(getApplicationContext(), MenuActivity.class);
-        startActivityForResult(myIntent, 0);
-        return true;
-
     }
 
 }
